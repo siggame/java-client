a32bd563e41cd2f4e960c9fbb9fb47a3b23618b3
Matthew <mrtf38@mst.edu>
2013-12-21 15:37:11 -0600

    Added network byte conversion helper

---
+++ templates/NetworkUtility.java
@@ -0,0 +1,55 @@
+import java.net.*;
+import java.io.*;
+import java.nio.ByteBuffer;
+
+public class NetworkUtility
+{
+  static int verbosity = 0;
+  
+  public String receive_string(Socket conn)
+  {
+    //read 4 bytes into length
+    byte[] length_data = byte[4];
+    conn.getInputStream().read(length_data, 0, 4);
+    //convert to int
+    int length = ByteBuffer.wrap(length_data).getInt();
+    
+    //read message bytes
+    byte[] message_data = byte[length];
+    conn.getInputStream().read(message_data, 0, 4);
+    String message = new String(message_data, "UTF-8");
+    vv_print("Recieved: " + message);
+    return message;
+  }
+  
+  public void send_string(Socket conn, String message)
+  {
+    vv_print("Sending: " + message);
+    //encode message
+    byte[] message_data = message.getBytes(Charset.forName("UTF-8"));
+    //Make header
+    byte[] length_data = ByteBuffer.allocate(4).putInt(message_data.length).array();
+    //Concatenate Arrays
+    byte[] sent_data = Arrays.copyOf(length_data, length_data.length + message_data.length);
+    System.arraycopy(message_data, 0, sent_data, length_data.length, message_data.length);
+    //send
+    conn.getOutputStream().write(sent_data);
+  }
+  
+  public void vv_print(String message)
+  {
+    if (verbosity > 1)
+    {
+      System.out.println(message);
+    }
+  }
+  
+  public void v_print(String message)
+  {
+    if (verbosity >= 1)
+    {
+      System.out.println(message);
+    }
+  }
+
+}
